f4d22feb95e55d8004e87fbb5bb4bcdb2d130ed5
oriolddm <orioldedios@gmail.com>
2018-10-04 12:04:27 +0200

    Profile task begin

---
+++ app/src/main/java/com/elloiro/userprofile/MainActivity.java
@@ -0,0 +1,90 @@
+package com.elloiro.userprofile;
+
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.bumptech.glide.Glide;
+import com.google.gson.Gson;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Profile profile;
+    private Gson gson;
+    private TextView name;
+    private TextView handle;
+    private TextView following;
+    private TextView followers;
+    private TextView about;
+    private ImageView background;
+    private ImageView profileImage;
+    private RequestQueue queue;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        gson = new Gson();
+        queue = Volley.newRequestQueue(this);
+
+        try {
+            InputStream stream = getAssets().open("Me.json");
+            InputStreamReader reader = new InputStreamReader(stream);
+            profile = gson.fromJson(reader, Profile.class);
+        } catch (IOException e) {
+            Toast.makeText(this,"No se puedo leer json", Toast.LENGTH_LONG).show();
+        }
+
+        name = findViewById(R.id.name);
+        handle = findViewById(R.id.handle);
+        following = findViewById(R.id.FollowingNumber);
+        followers = findViewById(R.id.FollowersNumber);
+        about = findViewById(R.id.AboutText);
+        background = findViewById(R.id.ImageBackgrownd);
+        profileImage = findViewById(R.id.ImageProfile);
+
+        updateProfile();
+
+
+
+//        StringRequest req = new StringRequest(Request.Method.GET,"http://www.omdbapi.com/?i=tt3896198&apikey=62e78dc5",new Response.Listener<String>(){
+//                    @Override
+//                    public void onResponse(String response) {
+//                        movie = gson.fromJson(response, Movie.class);
+//                        Glide.with(MovieActivity.this).load(movie.getPoster()).into(posterview);
+//                        updateProfile();
+//                    }
+//                }, new Response.ErrorListener(){
+//            @Override
+//            public void onErrorResponse(VolleyError error){
+//                Toast.makeText(MovieActivity.this,"Error de xarxa",Toast.LENGTH_LONG).show(); }
+//        });
+//
+//
+//        queue.add(req);
+    }
+    private void updateProfile() {
+        name.setText(profile.getName() + profile.getLastname());
+        handle.setText(profile.getHandle());
+        following.setText(profile.getFollowing());
+        followers.setText(profile.getFollowers());
+        about.setText(profile.getAbout());
+
+        Glide.with(this).load("file:///android_asset/Uri.png").into(profileImage);
+        Glide.with(this).load("file:///android_asset/UserProfile-background.jpg").into(background);
+
+    }
+
+}
---
+++ app/src/main/java/com/elloiro/userprofile/Profile.java
@@ -0,0 +1,30 @@
+package com.elloiro.userprofile;
+
+public class Profile {
+
+    private String name, lastname, handle, following, followers, about;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public String getHandle() {
+        return handle;
+    }
+
+    public String getFollowing() {
+        return following;
+    }
+
+    public String getFollowers() {
+        return followers;
+    }
+
+    public String getAbout() {
+        return about;
+    }
+}
